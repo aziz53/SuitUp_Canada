59d46bf46163adf75b321771478690eb86b73962
jamesball2018 <39163813+jamesball2018@users.noreply.github.com>
2018-06-05 14:38:31 -0400

    Update Accesories.java

--- Accesories.java
+++ Accesories.java
@@ -1,3 +1,5 @@
+import jdk.internal.util.xml.impl.Input;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -5,23 +7,28 @@ import javax.swing.*;
 public class Accesories extends JPanel implements ActionListener {
   GridBagConstraints gbc = new GridBagConstraints();
   
+    JButton purchase = new JButton("PURCHASE");
   JButton quit = new JButton("QUIT");
   
   public Accesories(){
     setLayout(new GridBagLayout());
     
+        purchase.addActionListener(this);
     quit.addActionListener(this);
     
     setLayout(new GridBagLayout());
     
     gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.insets = new Insets(20,50,50,20);
+    gbc.insets = new Insets(20,50,20,50);
     
-    gbc.weightx=1;
-    gbc.weighty=1;
-    gbc.ipady=0;
+    gbc.weightx=0;
+    gbc.weighty=0;
+
     gbc.gridx = 2;
     gbc.gridy = 0;
+    add(purchase,gbc);
+    gbc.gridx = 2;
+    gbc.gridy = 1;
     add(quit,gbc);
   }
   public void actionPerformed(ActionEvent event){
@@ -30,7 +37,11 @@ public class Accesories extends JPanel implements ActionListener {
     if(event.getSource() instanceof JButton){
       buttonPressed = (JButton)(event.getSource());
       
-      if(buttonPressed.equals(quit)){
+      if(buttonPressed.equals(purchase)){
+        System.exit(0);
+      }
+      
+      else if(buttonPressed.equals(quit)){
         System.exit(0);
       }
     }
