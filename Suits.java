4b0b86dd6b5ff6553aed855142d8b4b8c6b274c9
jamesball2018 <39163813+jamesball2018@users.noreply.github.com>
2018-06-05 14:40:52 -0400

    Update Suits.java

--- Suits.java
+++ Suits.java
@@ -1,3 +1,5 @@
+import jdk.internal.util.xml.impl.Input;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -5,21 +7,26 @@ import javax.swing.*;
 public class Suits extends JPanel implements ActionListener {
   GridBagConstraints gbc = new GridBagConstraints();
   
+  JButton purchase = new JButton("PURCHASE");
   JButton quit = new JButton("QUIT");
   
   public Suits(){
     setLayout(new GridBagLayout());
     
+    purchase.addActionListener(this);
     quit.addActionListener(this);
     
     gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.insets = new Insets(20,50,50,20);
+    gbc.insets = new Insets(20,50,20,50);
     
-    gbc.weightx=1;
+    gbc.weightx=0;
     gbc.weighty=1;
-    gbc.ipady=0;
-    gbc.gridx = 2;
+
+    gbc.gridx = 1;
     gbc.gridy = 0;
+    add(purchase,gbc);
+    gbc.gridx = 1;
+    gbc.gridy = 1;
     add(quit,gbc);
   }
   public void actionPerformed(ActionEvent event){
@@ -28,7 +35,11 @@ public class Suits extends JPanel implements ActionListener {
     if(event.getSource() instanceof JButton){
       buttonPressed = (JButton)(event.getSource());
       
-      if(buttonPressed.equals(quit)){
+      if(buttonPressed.equals(purchase)){
+        System.exit(0);
+      }
+      
+      else if(buttonPressed.equals(quit)){
         System.exit(0);
       }
     }
