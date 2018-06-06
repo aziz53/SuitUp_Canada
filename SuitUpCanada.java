f704cdbc9caee47e6a1f2c3cd3e64df0a2b59d76
jamesball2018 <39163813+jamesball2018@users.noreply.github.com>
2018-06-06 17:16:27 -0400

    Update SuitUpCanada.java

--- SuitUpCanada.java
+++ SuitUpCanada.java
@@ -1,40 +1,26 @@
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
-class SuitUpCanada extends JFrame{
-  SuitUpCanada(){
-    this.setVisible(true);
-    this.setSize(1000,1000);
-    this.add(new EnterPanel());
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SuitUpCanada extends JFrame {
+  
+  public static SuitUpCanada frame;
+  
+  public static void setContent(JPanel panel){
+    frame.setContentPane(panel);
+    frame.revalidate();
   }
-  class EnterPanel extends JPanel{
-    EnterPanel(){
-      JLabel menuTitle = new JLabel("Suit Up Canada");
-      JButton login = new JButton("Login/Register");
-      JButton aboutUs = new JButton("About Us");
-      ImageIcon menuPic = new ImageIcon(new ImageIcon("SuitPicture.jpg").getImage().getScaledInstance(500,500,Image.SCALE_DEFAULT));
-      JLabel menuLabel = new JLabel(menuPic);
-      setLayout(new GridBagLayout());
-      GridBagConstraints gbc = new GridBagConstraints();
-      gbc.insets = new Insets(20,50,50,20);
-      gbc.gridx = 0;
-      gbc.gridy = 0;
-      menuTitle.setFont(new Font("Serif", Font.BOLD, 24));
-      this.add(menuTitle,gbc);
-      gbc.gridx=3;
-      gbc.gridy=0;
-      this.add(menuLabel,gbc);
-      gbc.gridx=0;
-      gbc.gridy=1;
-      this.add(login,gbc);
-      gbc.gridx=0;
-      gbc.gridy=2;
-      this.add(aboutUs,gbc);
-      this.setSize(1000,1000);
-      this.setVisible(true);
-    }
+  
+  public SuitUpCanada(){
+    setTitle("Suit Up Canada");
+    setVisible(true);
+    setSize(1920, 1080);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
-  public static void main(String args[]){
-    SuitUpCanada mainGame = new SuitUpCanada();
+  
+  public static void main(String[] args) { 
+    System.out.println("> Starting Suit Up Canada...");
+    frame = new SuitUpCanada();
+    setContent(new UserSignIn());
   }
 }
