e3855935bb17df9bb62e82fc7347327f0db50e9c
jamesball2018 <39163813+jamesball2018@users.noreply.github.com>
2018-06-06 17:14:32 -0400

    Update Dresses.java

--- Dresses.java
+++ Dresses.java
@@ -1,5 +1,5 @@
 import jdk.internal.util.xml.impl.Input;
-
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,13 +7,13 @@ import javax.swing.*;
 public class Dresses extends JPanel implements ActionListener {
   GridBagConstraints gbc = new GridBagConstraints();
   
-    JButton purchase = new JButton("PURCHASE");
+  JButton previous = new JButton("PREVIOUS");
   JButton quit = new JButton("QUIT");
   
   public Dresses(){
     setLayout(new GridBagLayout());
     
-        purchase.addActionListener(this);
+    previous.addActionListener(this);
     quit.addActionListener(this);
     
     gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -21,12 +21,11 @@ public class Dresses extends JPanel implements ActionListener {
     
     gbc.weightx=0;
     gbc.weighty=0;
-
     gbc.gridx = 2;
-    gbc.gridy = 0;
-    add(purchase,gbc);
+    gbc.gridy = 2;
+    add(previous, gbc);
     gbc.gridx = 2;
-    gbc.gridy = 1;
+    gbc.gridy = 3;
     add(quit,gbc);
   }
   public void actionPerformed(ActionEvent event){
@@ -35,12 +34,11 @@ public class Dresses extends JPanel implements ActionListener {
     if(event.getSource() instanceof JButton){
       buttonPressed = (JButton)(event.getSource());
       
-      if(buttonPressed.equals(purchase)){
+      if(buttonPressed.equals(quit)){
         System.exit(0);
       }
-      
-      else if(buttonPressed.equals(quit)){
-        System.exit(0);
+      else if(buttonPressed.equals(previous)){
+        SuitUpCanada.setContent(new CustomerMenu());
       }
     }
   }
