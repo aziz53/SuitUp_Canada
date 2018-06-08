9ec0662e8c6b7c6eaacd3acc2e8b62be021de6e4
azizsafri14 <39163891+azizsafri14@users.noreply.github.com>
2018-06-08 17:26:10 -0400

    Add files via upload

---
+++ HumanResources.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;class HumanResources extends JPanel{
+  HumanResources(){
+    this.add(new JLabel("HR"));
+  }
+}
