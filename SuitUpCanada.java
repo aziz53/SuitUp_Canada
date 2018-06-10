066e57d9ac4ec6d04bc423b3b3d83ddf681addbf
azizsafri14 <39163891+azizsafri14@users.noreply.github.com>
2018-06-10 02:28:54 -0400

    Update SuitUpCanada.java

--- SuitUpCanada.java
+++ SuitUpCanada.java
@@ -2,17 +2,45 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.*;
  public class SuitUpCanada extends JFrame{
   public static SuitUpCanada mainGame;
   public static ArrayList<Employee> employeeList = new ArrayList<Employee>();
-  SuitUpCanada(){
-    employeeList.add(new Employee("Jerry's Dad", "1", "theBoss", "The Boss"));
+  public static String currentName;
+  public static String currentNum;
+  public static String currentPosition;
+  public static int currentStatus;
+  Scanner input;
+  SuitUpCanada() throws Exception{
+    input = new Scanner(new File("Employee Info.txt"));
+    while(input.hasNext()){
+      String total = input.nextLine();
+      String name = total.substring(0,total.indexOf(" "));
+      total = total.substring(total.indexOf(" ") + 1);
+      String lastName = total.substring(0,total.indexOf(" "));
+      name += " " + lastName;
+      System.out.println(name);
+      total = total.substring(total.indexOf(" ") + 1);
+      String number = total.substring(0,total.indexOf(" "));
+      total = total.substring(total.indexOf(" ") + 1);
+      String password = total.substring(0,total.indexOf(" "));
+      total = total.substring(total.indexOf(" ") + 1);
+      String position = total.substring(0,total.indexOf(" "));
+      total = total.substring(total.indexOf(" ") + 1);
+      System.out.println(position);
+      String salary = total.substring(0,total.indexOf(" "));
+      System.out.println(salary);
+      total = total.substring(total.indexOf(" ") + 1);
+      int status = Integer.parseInt(total);
+      employeeList.add(new Employee(name,number,password,position,salary,status));
+      System.out.println(number + " ");
+    }
     this.setVisible(true);
     this.setSize(1920,1080);
     this.setResizable(true);
     this.add(new EnterPanel());
   }
-  public static void main(String args[]){
+  public static void main(String args[]) throws Exception{
     mainGame = new SuitUpCanada();
   }
 }
