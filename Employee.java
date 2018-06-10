bd1b0b372858fa6fff6e39f36cb4baccfded8919
azizsafri14 <39163891+azizsafri14@users.noreply.github.com>
2018-06-10 02:26:58 -0400

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -3,11 +3,15 @@ class Employee{
   String number;
   String password;
   String position;
-  Employee(String name, String number, String password, String position){
+  String salary;
+  int status;
+  Employee(String name, String number, String password, String position, String salary, int status){
     this.name = name;
     this.number = number;
     this.password = password;
     this.position = position;
+    this.salary = salary;
+    this.status = status;
   }
   String getName(){
     return this.name;
@@ -17,5 +21,14 @@ class Employee{
   }
   String getPassword(){
     return this.password;
+  }
+  String getSalary(){
+    return this.salary;
+  }
+  String getPosition(){
+    return this.position;
+  }
+  int getStatus(){
+    return this.status;
   }
 }
