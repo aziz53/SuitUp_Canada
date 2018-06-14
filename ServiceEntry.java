579d21f06eb35b1f67d8caaa3c8e480a06456bc1
yujerry24 <yujerry24@gmail.com>
2018-06-13 23:19:40 -0400

    done v2?!

---
+++ ServiceEntry.java
@@ -0,0 +1,26 @@
+/**
+ * Auto Generated Java Class.
+ */
+ class ServiceEntry {
+  
+ String date;
+ String service;
+ 
+ ServiceEntry(String date, String service) {
+  this.date=date;
+  this.service=service;
+ }
+ 
+ String getDate() {
+  return this.date; 
+ }
+ 
+ String getService() {
+  return this.service; 
+ }
+ 
+ String toString1() {
+  return this.date+"|"+this.service; 
+ }
+  
+}
