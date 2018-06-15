8a4a7a479d1f1c3e174d5877e1c336c6fc4fa6d6
yujerry24 <yujerry24@gmail.com>
2018-06-14 20:08:03 -0400

    service entry object comment

--- ServiceEntry.java
+++ ServiceEntry.java
@@ -1,24 +1,34 @@
-/**
- * Auto Generated Java Class.
- */
+/*
+ * ServicceEntry.java
+ * This is the template for the service entry booking object
+ * Author: Aziz, James, Jerry
+ */ 
  class ServiceEntry {
   
- String date;
- String service;
+   //data the service entry has
+ private String date;
+ private String service;
  
+ //constructor
  ServiceEntry(String date, String service) {
   this.date=date;
   this.service=service;
  }
  
+ //getDate method
+ //@returns the date of the ServiceEntry object
  String getDate() {
   return this.date; 
  }
  
+ //getService method
+ //@returns the type of service of the ServiceEntry object
  String getService() {
   return this.service; 
  }
  
+ //toString1 method
+ //@returns a custom toString method that displays all the data of the object 
  String toString1() {
   return this.date+"|"+this.service; 
  }
